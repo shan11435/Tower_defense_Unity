c941daa20c2ac947e58fba3deb103f168376bfd7
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-10-29 14:31:23 -0400

    Added movement
    edited the ground to make it look grassy, and made the player move smoothly

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,8 +6,8 @@ public class EnemyMover : MonoBehaviour
 {
     //creates a list using the waypoint class
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-
-    [SerializeField] private float waitTime = 1f;
+    //this can edit how fast the object travels, this also fixes the issue if we accidentally put a negative number so the games doesn't break
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +21,24 @@ public class EnemyMover : MonoBehaviour
         //for loop for the waypoint and looping through the list
         foreach (Waypoint waypoint in path)
         {
-            //this will move the enemy object
-            transform.position = waypoint.transform.position;
-            //it will stop for 1 second and restart the method until the loop is finished, and it will no longer run the loop
-            yield return new WaitForSeconds(waitTime);
+            
+            Vector3 startPosition = transform.position;
+            //this is the coordinates for the ppints in the waypoint editor
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            
+            //this is responsible for the object to turn and face where they're moving
+            transform.LookAt(endPosition);
+
+            //while the object reaches 1 travel percent
+            while (travelPercent < 1f)
+            {
+                //this is responsible for player speed
+                travelPercent += Time.deltaTime * speed;
+                //this will cause the object to move smoothly
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
