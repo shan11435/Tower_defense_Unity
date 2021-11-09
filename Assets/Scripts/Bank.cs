098ea3375dbaa1bd66546b13e3a70d540ad0c31d
Tahsin Ahmed <shanahmed1997@gmail.com>
2021-11-09 16:47:12 -0500

    added difficulty function

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     //hiw much the player will start having
     [SerializeField] int startingBalance = 150;
-
     [SerializeField] int currentBalance = 150;
-
+    //this will make it possible to edit the text UI
+    [SerializeField] TextMeshProUGUI displayBalance;
+    
     //how much money the player currently has
     public int CurrentBalance
     {
@@ -20,6 +22,8 @@ public class Bank : MonoBehaviour
     {
         //this will make the players balance to the starting balance which is 150
         currentBalance = startingBalance;
+        //change the text UI
+        UpdateDisplay();
     }
 
     //this will add money into the players account
@@ -27,17 +31,28 @@ public class Bank : MonoBehaviour
     {
         //Mathf.abs will convert negative numbers into positive just incase if player deposits a negative value
         currentBalance += Mathf.Abs(amount);
+        //change the text UI
+        UpdateDisplay();
     }
 
     //this will subtract the money in the players account
     public void Withdrawal(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        //change the text UI
+        UpdateDisplay();
         if (currentBalance < 0)
         {
             //lose the game
             ReloadScene();
         }
+    }
+
+    //this will change the text based on the current balance
+    void UpdateDisplay()
+    {
+        //this will cause the player to see how much gold he has
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     //this will restart the game
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,9 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//when you drag this script into unity inspector, it will also automatically add the enemy script file (this file depends on enemy script file)
+[RequireComponent(typeof(Enemy))]
+
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
+    
+    //adding health to enemy after death
+    [SerializeField] int difficultyRamp = 1;
     [SerializeField] int currentHitPoints = 0;
 
     //start the process of calling the bank c# script file
@@ -37,6 +43,8 @@ public class EnemyHealth : MonoBehaviour
         {
             //this will disbable the enemy gameobject to be reused again in the future
             gameObject.SetActive(false);
+            //this will increase the enemy health to 1 everytime it dies
+            maxHitPoints += difficultyRamp;
             //will add gold into players account, it's blank because in enemy script it's adding 25 gold
             enemy.RewardGold();
         }
